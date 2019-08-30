bf14ba386376f5e977a197545f44765c80a46c44
Cynthiamf <c_martiny@outlook.com>
2019-08-30 12:20:54 +0200

    readme

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
-# npx-card
+<h1 align="center">Welcome to Npx V-Card 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.2.4-blue.svg?cacheSeconds=2592000" />
+  <a href="https://github.com/Cynthiamf/npx-card#readme">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" target="_blank" />
+  </a>
+  <a href="https://github.com/Cynthiamf/npx-card/graphs/commit-activity">
+    <img alt="Maintenance" src="https://img.shields.io/badge/Maintained%3F-yes-green.svg" target="_blank" />
+  </a>
+  <a href="https://github.com/Cynthiamf/npx-card/blob/master/LICENSE">
+    <img alt="License: ISC" src="https://img.shields.io/badge/License-ISC-yellow.svg" target="_blank" />
+  </a>
+</p>
 
-To see my v-card : 
-_in terminal_ `npx cynthiamf`
+> V-card
+
+### 🏠 [Homepage](https://www.npmjs.com/package/cynthiamf)
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+```sh
+npx cynthiamf
+```
+
+## Run tests
+
+```sh
+npm run test
+```
+
+## Author
+
+👤 **Cynthia M.**
+
+* Github: [@Cynthiamf](https://github.com/Cynthiamf)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/Cynthiamf/npx-card/issues).
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+## 📝 License
+
+Copyright © 2019 [Cynthia M.](https://github.com/Cynthiamf).<br />
+This project is [ISC](https://github.com/Cynthiamf/npx-card/blob/master/LICENSE) licensed.
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
--- card.js
+++ card.js
@@ -6,6 +6,9 @@ const axios = require('axios');
 const chalk = require('chalk');
 const boxen = require('boxen');
 const path = require('path');
+const QRCode = require('qrcode-terminal');
+const prompt = require('prompt');
+var inquirer = require('inquirer');
 
 const options = {
     padding: 3,
@@ -14,20 +17,23 @@ const options = {
     borderColor: '#FF9999',
 };
 
+
 const data = {
     name: chalk.white('               Cynthia Martiny'),
     symbol: chalk.hex('#66B2FF')('▲'),
     handle: chalk.hex('#FF9999').bold('Cynthiamf'),
-    work: chalk.white('Junior Developer Web ' + chalk.hex('#FF9999')('@') + 'BeCode'),
+    work: chalk.white('Junior Web Developer ') + chalk.hex('#FF9999')('@BeCode'),
     twitter: chalk.hex('#66B2FF')('https://twitter.com/CynthiaMartiny'),
     github: chalk.hex('#66B2FF')('https://github.com/Cynthiamf'),
     linkedin: chalk.hex('#66B2FF')('https://linkedin.com/in/CynthiaMartiny'),
-    npx: chalk.white('npx cynthia'),
+    npm: 'https://www.npmjs.com/' + chalk.hex('#FF9999')('~cynthiamf'),
+    npx: chalk.white('npx cynthia-npx'),
     nameWork: chalk.hex('#FF9999').bold('      Work:'),
     nameTwitter: chalk.hex('#FF9999').bold('   Twitter:'),
     nameGitHub: chalk.hex('#FF9999').bold('    GitHub:'),
     nameLinkedIn: chalk.hex('#FF9999').bold('  LinkedIn:'),
-    nameCard: chalk.hex('#FF9999').bold('      Card:')
+    nameNpm: chalk.hex('#FF9999').bold('       Npm:'),
+    nameCard: chalk.hex('#FF9999').bold('      Card:'),
 }
 
 const newline = '\n'
@@ -36,14 +42,43 @@ const working = `${data.nameWork}  ${data.work}`
 const twittering = `${data.nameTwitter}  ${data.twitter}`
 const githubing = `${data.nameGitHub}  ${data.github}`
 const linkedining = `${data.nameLinkedIn}  ${data.linkedin}`
+const npming = `${data.nameNpm}  ${data.npm}`
 const carding = `${data.nameCard}  ${data.npx}`
+
 
 const output = heading +
     newline + newline +
     working + newline +
     twittering + newline +
     githubing + newline +
-    linkedining + newline + newline +
+    linkedining + newline +
+    npming + newline + newline +
     carding
 
+
+
+
+
 console.log((boxen(output, options)));
+
+inquirer
+    .prompt([{
+            type: 'list',
+            name: 'qrcode',
+            message: chalk.hex('#FF9999').bold('Would you like to see the QRCode to access to my resume ?'),
+            choices: ['Yes', 'No']
+        }
+
+    ])
+    .then(answers => {
+        if (answers.qrcode === 'Yes') {
+            const qr = QRCode.generate('https://www.slideshare.net/slideshow/embed_code/key/iyoSEqPxqWD8vD', {
+                small: true,
+            }, function (QRCode) {
+                console.log(QRCode);
+            })
+        } else {
+            console.log('Ok');
+        };
+    });
+console.log();
